3552cafeed2c45a424f679a5ac80e0505dac477a
Jonathan Kim <jonathan.sj.kim@gmail.com>
2018-12-01 16:23:08 -0800

    number 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log("Hello");
 console.log("Bye");
-console.console.log("Calvin");
-console.console.log("Calvin");
-console.console.log("Calvin");
-console.console.log("Calvin");
+console.log("Calvin");
+console.log("Calvin");
+console.log("Calvin");
+console.log("Calvin");
+console.log("Jon");
+console.log("Ian");
